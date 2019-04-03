aac965cb34802315c958e717b94e1d24487b170b
Susan Tan <ssusantan9@gmail.com>
2019-04-03 05:58:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,3 @@
-class Person
- 
-#   def initialize(name)
-#     @name = name
-#   end
- 
-#   def name
-#     @name
-#   end
- 
-#   def name=(new_name)
-#     @name = new_name
-#   end
- 
-# end
-
 class Dog 
   
 def breed
