2a37812a0f8ad64cb6c1d0cb32abf46e4d7c6ab0
Susan Tan <ssusantan9@gmail.com>
2019-04-03 05:57:52 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+ 
+  def name(name)
+    @name = name
+  end
+ 
+ 
+  def name=(new_name)
+    @new_name = new_name
+  end
+  
+ 
+  def job
+    @job = job
+  end
+ 
+ 
+  def job=(new_job)
+    @new_job = new_job
+  end
+ 
+end
